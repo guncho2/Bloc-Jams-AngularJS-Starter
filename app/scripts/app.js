89c3c2efa681f4e44d286735b71ba85252229d60
Eduardo Lowensohn <gunchosoft@gmail.com>
2018-01-11 12:34:31 -0500

    bloc jams2

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+angular.module('blocJams', [])
+//this angular.module will act as a root for different parts of the application
+//blocjams is the name of the module and the empty array injects independency
+//into an application
