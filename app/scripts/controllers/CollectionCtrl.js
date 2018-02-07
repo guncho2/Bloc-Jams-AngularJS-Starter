98ec7a085be13aa236f9a8c738af95a1e37286c1
Eduardo Lowensohn <gunchosoft@gmail.com>
2018-02-07 10:46:08 -0500

    direcives2-4

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -10,7 +10,7 @@ this.albums = Fixtures.getCollection(12);
   //   for (var i=0; i < 12; i++) {
     //     this.albums.push(angular.copy(albumPicasso));
      }
-}
+
      angular
          .module('blocJams')
          .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
